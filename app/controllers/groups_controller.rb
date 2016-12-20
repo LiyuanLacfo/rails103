ddbc3925df1b30e4ae8ed3c0c8acb7570604cbb5
lacfo <liyuanlacfo1990@gmail.com>
2016-12-20 19:51:22 +0800

    Implement group#new and group#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,25 @@ class GroupsController < ApplicationController
 	def index
 		@groups = Group.all
 	end
+
+	def new
+		@group = Group.new
+	end
+
+	def create
+		@group = Group.new(group_params)
+		if @group.save
+			redirect_to groups_path
+			flash[:notice] = "Created Successfully"
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def group_params
+		params.require(:group).permit(:title, :description)
+	end
+
 end
