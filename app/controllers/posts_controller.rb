3179d9dddb78ca497ee34e236e3b33d6bda42732
lacfo <liyuanlacfo1990@gmail.com>
2016-12-21 15:46:18 +0800

    Add my posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,32 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+		@group = Group.find(params[:group_id])
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@group = Group.find(params[:group_id])
+		@post = Post.find(params[:id])
+		@post.user = current_user
+		@post.group = @group
+		if @post.update(post_params)
+			redirect_to account_posts_path(@group)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		flash[:alert] = "Post deleted"
+		redirect_to account_posts_path
+	end
+
 	private
-	
+
 	def post_params
 		params.require(:post).permit(:content)
 	end
